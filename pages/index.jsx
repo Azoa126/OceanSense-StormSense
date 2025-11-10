90a7a21ef38cd3a6f74ae29e322b5d306f76d3bd
Azoa126 <ajilazoa126@gmail.com>
2025-11-10 09:28:01 +0530

    Delete pages directory

--- pages/index.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "40px" }}>
-      <h1>🌊 OceanSense-StormSense</h1>
-      <p>Welcome to the ocean data visualization dashboard.</p>
-    </div>
-  );
-}
