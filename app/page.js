7b8169868ba41db9daf3ee5e42a2b46b1fbac4c7
Azoa126 <ajilazoa126@gmail.com>
2025-11-11 10:17:31 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 import dynamic from "next/dynamic";
-const Neritic_v2 = dynamic(() => import("../components/Neritic_v2"), { ssr: false });
+
+const Neritic_v2 = dynamic(() => import("../Neritic_v2"), { ssr: false });
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-3xl font-bold text-center mt-6">OceanSense 🌊</h1>
+    <main className="p-6">
+      <h1 className="text-3xl font-bold text-center mt-6">
+        OceanSense 🌊
+      </h1>
       <Neritic_v2 />
     </main>
   );
